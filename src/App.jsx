a2e150cab56e6431e53a1724471599bb994042a9
Dayer <93074807+dayercher005@users.noreply.github.com>
2025-10-18 22:41:11 +0800

    Debugging

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Home from './components/home/Home.jsx'
 import About from './components/about/About.jsx'
 import Projects from './components/projects/Projects.jsx'
 import Skills from './components/skills/Skills.jsx'
-import Contact from './components/contact/Contact.jsx'
 import './App.css'
 
 function App() {
